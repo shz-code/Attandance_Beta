903f3ebcef40a04a61255b22bf3dfc2f3b264d8d
shz-cod <web.shahidul.alam@gmail.com>
2022-09-22 18:36:46 +0600

    Last part resolved

--- index.js
+++ index.js
@@ -1,336 +1,62 @@
+// Get all the buttons as Nodelist
+let presentBtns = document.querySelectorAll(".p");
+let absentBtns = document.querySelectorAll(".a");
 
-let g1 = document.getElementById("g-1");
-let r1 = document.getElementById("r-1"); 
-g1.addEventListener("click", () =>{
-        g1.classList.add("g-light");
-        r1.classList.remove("r-light");
-    });
-r1.addEventListener("click", () =>{
-    r1.classList.add("r-light");
-    g1.classList.remove("g-light");
-});
+// Function to count all present and absent student as well as percentage
+const countAttendence = ()=> {
+    // Get all the required tags 
+    let allPresents = document.querySelectorAll(".presentFlag")
+    let allAbsents = document.querySelectorAll(".absantFlag");
+    let presentShow = document.getElementById("pt");
+    let absentShow = document.getElementById("at");
+    let presentPercentageShow = document.getElementById("pp");
+    let absentPercentageShow = document.getElementById("ap");
 
+    // Convert string to integer for calculation
+    let presentCount = parseInt(allPresents.length);
+    let absentCount = parseInt(allAbsents.length);
 
-let g2 = document.getElementById("g-2");
-let r2 = document.getElementById("r-2"); 
-g2.addEventListener("click", () =>{
-        g2.classList.add("g-light");
-        r2.classList.remove("r-light");
-    });
-r2.addEventListener("click", () =>{
-    r2.classList.add("r-light");
-    g2.classList.remove("g-light");
-});
+    // Updating DOM
+    presentShow.innerHTML = presentCount;
+    absentShow.innerHTML = absentCount;
 
-let g3 = document.getElementById("g-3");
-let r3 = document.getElementById("r-3"); 
-g3.addEventListener("click", () =>{
-        g3.classList.add("g-light");
-        r3.classList.remove("r-light");
-});
-r3.addEventListener("click", () =>{
-    r3.classList.add("r-light");
-    g3.classList.remove("g-light");
-});
+    // Calculating Percentage
+    let totalCount = presentCount + absentCount;
+    let per_Present = parseInt((presentCount / totalCount) * 100) ;
+    let per_Absent = parseInt((absentCount / totalCount) * 100) ;
 
-let g4 = document.getElementById("g-4");
-let r4 = document.getElementById("r-4"); 
-g4.addEventListener("click", () =>{
-    g4.classList.add("g-light");
-    r4.classList.remove("r-light");
-});
-r4.addEventListener("click", () =>{
-r4.classList.add("r-light");
-g4.classList.remove("g-light");
-});
+    // Updating DOM
+    presentPercentageShow.innerHTML = `${per_Present}%`;
+    absentPercentageShow.innerHTML = `${per_Absent}%`;
+}
 
-let g5 = document.getElementById("g-5");
-let r5 = document.getElementById("r-5");
-g5.addEventListener("click", () =>{
-    g5.classList.add("g-light");
-    r5.classList.remove("r-light");
-});
-r5.addEventListener("click", () =>{
-    r5.classList.add("r-light");
-    g5.classList.remove("g-light");
-});   
-    
-let g6 = document.getElementById("g-6");
-let r6 = document.getElementById("r-6");
-g6.addEventListener("click", () =>{
-    g6.classList.add("g-light");
-    r6.classList.remove("r-light");
-});
-r6.addEventListener("click", () =>{
-    r6.classList.add("r-light");
-    g6.classList.remove("g-light");
-});   
-
-let g7 = document.getElementById("g-7");
-let r7 = document.getElementById("r-7");
-g7.addEventListener("click", () =>{
-    g7.classList.add("g-light");
-    r7.classList.remove("r-light");
-});
-r7.addEventListener("click", () =>{
-    r7.classList.add("r-light");
-    g7.classList.remove("g-light");
-});   
-
-let g8 = document.getElementById("g-8");
-let r8 = document.getElementById("r-8");
-g8.addEventListener("click", () =>{
-    g8.classList.add("g-light");
-    r8.classList.remove("r-light");
-});
-r8.addEventListener("click", () =>{
-    r8.classList.add("r-light");
-    g8.classList.remove("g-light");
-});   
-
-let g9 = document.getElementById("g-9");
-let r9 = document.getElementById("r-9");
-g9.addEventListener("click", () =>{
-    g9.classList.add("g-light");
-    r9.classList.remove("r-light");
-});
-r9.addEventListener("click", () =>{
-    r9.classList.add("r-light");
-    g9.classList.remove("g-light");
-});   
-
-let g10 = document.getElementById("g-10");
-let r10 = document.getElementById("r-10");
-g10.addEventListener("click", () =>{
-    g10.classList.add("g-light");
-    r10.classList.remove("r-light");
-});
-r10.addEventListener("click", () =>{
-    r10.classList.add("r-light");
-    g10.classList.remove("g-light");
-});   
-
-let g11 = document.getElementById("g-11");
-let r11 = document.getElementById("r-11");
-g11.addEventListener("click", () =>{
-    g11.classList.add("g-light");
-    r11.classList.remove("r-light");
-});
-r11.addEventListener("click", () =>{
-    r11.classList.add("r-light");
-    g11.classList.remove("g-light");
-});   
-
-let g12 = document.getElementById("g-12");
-let r12 = document.getElementById("r-12");
-g12.addEventListener("click", () =>{
-    g12.classList.add("g-light");
-    r12.classList.remove("r-light");
-});
-r12.addEventListener("click", () =>{
-    r12.classList.add("r-light");
-    g12.classList.remove("g-light");
-});   
-
-let g13 = document.getElementById("g-13");
-let r13 = document.getElementById("r-13");
-g13.addEventListener("click", () =>{
-    g13.classList.add("g-light");
-    r13.classList.remove("r-light");
-});
-r13.addEventListener("click", () =>{
-    r13.classList.add("r-light");
-    g13.classList.remove("g-light");
-});   
-
-let g14 = document.getElementById("g-14");
-let r14 = document.getElementById("r-14");
-g14.addEventListener("click", () =>{
-    g14.classList.add("g-light");
-    r14.classList.remove("r-light");
-});
-r14.addEventListener("click", () =>{
-    r14.classList.add("r-light");
-    g14.classList.remove("g-light");
-});   
-
-let g15 = document.getElementById("g-15");
-let r15 = document.getElementById("r-15");
-g15.addEventListener("click", () =>{
-    g15.classList.add("g-light");
-    r15.classList.remove("r-light");
-});
-r15.addEventListener("click", () =>{
-    r15.classList.add("r-light");
-    g15.classList.remove("g-light");
-});   
-
-// // let count=0;
-// // function c(event) {
-// //     if(event.target.id === 'rock'){
-// //         let k = document.getElementById("pt");
-// //           count += 1;
-// //           let g= count;
-// //           k.innerHTML = g;
-// //         };
-// //     }
-
-// var PC = 0;
-// var AC = 0;
-
-// function go1(num){
-//     if(num==1) {
-//         return PC++,AC--;
-//  }
-//  else if(num == 0){
-//      return PC--,AC++;
-
-//  }
-// }
-// function go2(num){
-//     if(num==1) {
-//            return PC++,AC--;
-//     }
-//     else if(num == 0){
-//         return PC--,AC++;
-
-//     }
-
-// }
-// function go3(num){
-//    if(num==1) {
-//           return PC++,AC--;
-//     }
-//     else if(num == 0){
-//         return PC--,AC++;
-
-//     }
-
-// }
-// function go4(num){
-//    if(num==1) {
-//           return PC++,AC--;
-//     }
-//     else if(num == 0){
-//         return PC--,AC++;
-
-//     }
-
-// }
-// function go5(num){
-//    if(num==1) {
-//           return PC++,AC--;
-//     }
-//     else if(num == 0){
-//         return PC--,AC++;
-
-//     }
-
-// }
-// function go6(num){
-//    if(num==1) {
-//           return PC++,AC--;
-//     }
-//     else if(num == 0){
-//         return PC--,AC++;
-
-//     }
-
-// }
-// function go7(num){
-//    if(num==1) {
-//           return PC++,AC--;
-//     }
-//     else if(num == 0){
-//         return PC--,AC++;
-
-//     }
-
-// }
-// function go8(num){
-//    if(num==1) {
-//           return PC++,AC--;
-//     }
-//     else if(num == 0){
-//         return PC--,AC++;
-
-//     }
-
-// }
-// function go9(num){
-//    if(num==1) {
-//           return PC++,AC--;
-//     }
-//     else if(num == 0){
-//         return PC--,AC++;
-
-//     }
-
-// }
-// function go10(num){
-//    if(num==1) {
-//           return PC++,AC--;
-//     }
-//     else if(num == 0){
-//         return PC--,AC++;
-
-//     }
-
-// }
-// function go11(num){
-//    if(num==1) {
-//           return PC++,AC--;
-//     }
-//     else if(num == 0){
-//         return PC--,AC++;
-
-//     }
-// console.log(PC,AC);
-
-
-// }
-// function go12(num){
-//    if(num==1) {
-//           return PC++,AC--;
-//     }
-//     else if(num == 0){
-//         return PC--,AC++;
-
-//     }
-
-// }
-// function go13(num){
-//    if(num==1) {
-//           return PC++,AC--;
-//     }
-//     else if(num == 0){
-//         return PC--,AC++;
-
-//     }
-
-// }
-// function go14(num){
-//    if(num==1) {
-//           return PC++,AC--;
-//     }
-//     else if(num == 0){
-//         return PC--,AC++;
-
-//     }
-
-// }
-// function go15(num){
-//     if(num==1) {
-// console.log(PC,AC);
-        
-//        return PC++,AC--;
-
-//  }
-//  else if(num == 0){
-// console.log(PC,AC);
-
-//      return PC--,AC++;
-
-//  }
-// }
-
+// From Nodelist we get one single item inside the forEach function
+presentBtns.forEach((btn) => {
+    btn.addEventListener("click" , () => {
+        btn.classList.add("g-light")
+        // Get the parent element / <tr> tag 
+        let studentInfo = btn.parentElement.parentElement;
+        // Add a class to mark student as present
+        studentInfo.classList.add("presentFlag");
+        // Check if student is already absent
+        if(btn.parentElement.parentElement.classList.contains("absantFlag")){
+            studentInfo.classList.remove("absantFlag")
+            studentInfo.querySelector(".r-light").classList.remove("r-light");
+        }
+        countAttendence() // Call countAttendence function
+    })
+})
+absentBtns.forEach((btn) => {
+    btn.addEventListener("click" , () => {
+        btn.classList.add("r-light")
+        let studentInfo = btn.parentElement.parentElement;
+        // Add a class to mark student as absent
+        studentInfo.classList.add("absantFlag");
+        // Check if student is already present
+        if(btn.parentElement.parentElement.classList.contains("presentFlag")){
+            studentInfo.classList.remove("presentFlag")
+            studentInfo.querySelector(".g-light").classList.remove("g-light");
+        }
+        countAttendence() // call countAttendence function
+    })
+})
